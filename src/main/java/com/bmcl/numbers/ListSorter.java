ff2dfa2d9553333d87525fa8fa4a4918818f1b78
Diogo Teixeira <A044483@umaia.pt>
2024-10-15 22:06:22 +0100

    9

--- src/main/java/com/bmcl/numbers/ListSorter.java
+++ src/main/java/com/bmcl/numbers/ListSorter.java
@@ -8,11 +8,16 @@ import java.util.List;
  * An utility class to sort list of numbers.
  */
 public class ListSorter implements GenericListSorter {
-
-    @Override
     public List<Integer> sort(List<Integer> list) {
-        List<Integer> sorted = new ArrayList<>(list);
-        Collections.sort(sorted);
+        List<Integer> sorted = new ArrayList<>();
+        for (Integer number : list)
+            sorted.add(number);
+
+        for (int i = 0; i < sorted.size() -1 ; i++)
+            for (int j = i + 1; j < sorted.size(); j++)
+                if (sorted.get(i) > sorted.get(j))
+                    Collections.swap(sorted, i, j);
+
         return sorted;
     }
 }
